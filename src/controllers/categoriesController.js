486ef1e2c336a389ca5bddbb22502121cb5902c9
owk4rce <owk4rce@gmail.com>
2024-10-19 21:45:13 +0300

    added routes for categories and orders

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,12 +1,12 @@
 import Category from "../models/Category"
 
-async function getAllCats(req, res, next) {
+async function getAllCategories(req, res, next) {
     try{
-        const cats = await Category.find({})
-        if (cats.length === 0) 
+        const categories = await Category.find({})
+        if (categories.length === 0) 
            return res.status(404).json({ message: "No categories found" })      
         
-        res.status(200).json(cats)
+        res.status(200).json(categories)
 
     } catch(error) {
         res.status(500).json({ message: error.message })
@@ -14,27 +14,27 @@ async function getAllCats(req, res, next) {
     
 }
 
-async function getCatById(req, res, next) {
+async function getCategoryById(req, res, next) {
     try {
         const { id } = req.params
-        const cat = await Category.findById(id)
+        const category = await Category.findById(id)
 
-        if (!cat)
+        if (!category)
            return res.status(404).json({ message: "Category not found" })
         
-        res.status(200).json(cat)
+        res.status(200).json(category)
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
 
-async function createCat(req, res, next) {
+async function createCategory(req, res, next) {
     try {
         const {name, description} = req.body
         if(!name)
             return res.status(400).json({ message: "Category name is missing" })
-        const newCat = Category({name, description})
-        await newCat.save()
+        const newCategory = Category({name, description})
+        await newCategory.save()
 
     } catch(error) {
         res.status(500).json({ message: error.message })
@@ -42,39 +42,39 @@ async function createCat(req, res, next) {
     
 }
 
-async function updateCatDescr(req, res, next) {
+async function updateCategoryDescr(req, res, next) {
     try {
         const { id } = req.params
         const {description} = req.body
         
-        const cat = await Category.findById(id)
+        const category = await Category.findById(id)
 
-        if (!cat)
+        if (!category)
            res.status(404).json({ message: "Category not found" })
 
         if (!description)
             res.status(400).json({ message: "Description is missing" })
 
-        if (description === cat.description)
+        if (description === category.description)
             res.status(400).json({ message: "There is nothing to change" })
 
-        await cat.updateDescription(description)
-        await cat.save()
+        await category.updateDescription(description)
+        await category.save()
         res.status(200).json({ message: "Description updated successfully"});
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
 
-async function deleteCat(req, res, next) {
+async function deleteCategory(req, res, next) {
     try {
         const { id } = req.params
         if(!id)
             res.status(400).json({ message: "Category ID is missing" })
        
-        const deletedCat = await Category.findByIdAndDelete(id)
+        const deletedCategory = await Category.findByIdAndDelete(id)
 
-        if (!deletedCat)
+        if (!deletedCategory)
             res.status(404).json({ message: "Category not found" });
         
         res.status(204).send();
@@ -86,10 +86,10 @@ async function deleteCat(req, res, next) {
 }
 
 module.exports = {
-    getAllCats,
-    getCatById,
-    createCat,
-    updateCatDescr,
-    deleteCat
+    getAllCategories,
+    getCategoryById,
+    createCategory,
+    updateCategoryDescr,
+    deleteCategory
   };
   
