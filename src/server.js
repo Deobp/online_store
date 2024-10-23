0d12e51287ef81ab91ea29f93eb8d62fcbaebf31
owk4rce <owk4rce@gmail.com>
2024-10-24 00:22:42 +0300

    registration added, some minor fixes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
-dotenv.config({path: "./config/.env"})
+dotenv.config({path: "./src/config/.env"})
 import express from "express";
 import connectDb from "./config/db.js"
 import cors from 'cors'
 import helmet from 'helmet'
 import categoriesRoutes from "../src/routes/categoriesRoutes.js"
 import ordersRoutes from "../src/routes/ordersRoutes.js"
-
+import userRoute from "../src/routes/userRoute.js"
 
 
 const app = express()
@@ -19,6 +19,7 @@ app.use(express.json())
 
 app.use("/api/categories", categoriesRoutes)
 app.use("/api/orders", ordersRoutes)
+app.use("/api/users", userRoute)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on ${process.env.PORT}`)
